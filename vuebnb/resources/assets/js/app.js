d5aa7d54558711de6e62e608e23f08c8b045748f
Htoo Maung Thait <htoo.mt@blueplanet.com.mm>
2019-03-04 18:41:45 +0630

    header image component abstractiong

--- vuebnb/resources/assets/js/app.js
+++ vuebnb/resources/assets/js/app.js
@@ -4,6 +4,7 @@ import "core-js/fn/object/assign";
 //import MyComponent from "./components/MyComponent.vue";
 import ImageCarousel from './components/ImageCarousel.vue';
 import ModalWindow from './components/ModalWindow.vue';
+import HeaderImage from './components/HeaderImage.vue';
 let model = JSON.parse(window.vuebnb_listing_model);
 model = populateAmenitiesAndPrices(model);
 
@@ -15,10 +16,7 @@ var app = new Vue({
         data: Object.assign(model, {
             title: model.title,
             address: model.address,
-            about: model.about,
-		headerImageStyle: {
-		    'background-image': `url(${model.images[0]})`
-		},
+            about: model.about,		
             amenities: model.amenities,
             prices: model.prices,
             contracted: true,
@@ -31,34 +29,24 @@ var app = new Vue({
             }
         },
 	methods: {
-		escapeKeyListener(evt) {
-		  if (evt.keyCode === 27 && app.modalOpen) {
-		    app.modalOpen = false;
-		  }
-		},
+            
             openModal() {
                 this.$refs.imagemodal.modalOpen = true;
             },
 	},
 	watch : {
-		modalOpen : function() {
-		    var className = 'modal-open';
-		      if (this.modalOpen) {
-		        document.body.classList.add(className);
-		      } else {
-		        document.body.classList.remove(className);
-		      }
-		}
+            
 	},
 	created: function() {
-	    document.addEventListener('keyup', this.escapeKeyListener);
+	    
 	  },
 	destroyed: function () {
-	    document.removeEventListener('keyup', this.escapeKeyListener);
+	    
 	},
         components:{
             'image-carousel' : ImageCarousel,
-            'modal-window' : ModalWindow
+            'modal-window' : ModalWindow,
+            'header-image' : HeaderImage
         }
 });
 
